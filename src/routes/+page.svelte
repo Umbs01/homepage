f94cdb6a7a6afcbf1943aea8add0c8a1838b0e09
Umbs01 <overpowerpotato0@gmail.com>
2024-12-17 21:05:07 +0700

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Typed from 'typed.js';
+	import { onMount } from 'svelte';
+
+	export let strings = ['send noods'];
+	export let typeSpeed = 100;
+	export let backSpeed = 75;
+
+	/**
+	 * @type {Typed}
+	 */
+	let typed;
+
+	onMount(() => {
+		// Initialize Typed.js after DOM is ready
+		typed = new Typed('.auto-type', {
+			strings: strings,
+			typeSpeed: typeSpeed,
+			backSpeed: backSpeed,
+			loop: true,
+			showCursor: true,
+			cursorChar: '█'
+		});
+
+		// Cleanup Typed.js on component unmount
+		return () => {
+			typed.destroy();
+		};
+	});
+</script>
+
+<svelte:head>
+	<title>send noods</title>
+</svelte:head>
+
+<main>
+	<div class="flex h-screen flex-col items-center justify-center space-y-4">
+		<h1 class="text-7xl font-bold">
+			<div>
+				<b>&#36;</b>&#62;
+				<p class="auto-type inline align-baseline font-mono leading-none"></p>
+			</div>
+		</h1>
+	</div>
+</main>
