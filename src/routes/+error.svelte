b766d131063827ac91db69efe8969d8cfb77dae2
Umbs01 <overpowerpotato0@gmail.com>
2024-12-17 21:06:35 +0700

    a

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>404 | Not Found</title>
+</svelte:head>
+
+<div class="flex h-screen items-center justify-center">
+	<div>
+		<p class="text-center text-6xl">404 | Not Found</p>
+		<br />
+		<p class="text-center text-xl">The page you are looking for does not exist.</p>
+	</div>
+</div>
